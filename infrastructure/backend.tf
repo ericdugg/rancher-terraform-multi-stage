bed6bbb3223230145880b2adac2370a1780ee50a
eduggan <eduggan@oss-group.co.nz>
2020-05-04 15:38:33 +1200

    no vars in backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     bucket = "oss-lab-terraform"
     key    = "lab/ED/rancher-terraform-multi-stage/infrastructure"
-    region = var.aws_region
-    profile = var.aws_profile
-    role_arn  = var.aws_role_arn
+    region = "ap-southeast-2"
+    profile = "oss"
+    role_arn  = "arn:aws:iam::085032814280:role/OSS_Terraform_no_MFA"
     dynamodb_table = "terraform_s3_statelock"
   }
 }
