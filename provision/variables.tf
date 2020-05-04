f7873c9aa0c693c45b65e98e92e199a770798851
eduggan <eduggan@oss-group.co.nz>
2020-05-04 14:56:32 +1200

    fix ansible

---
+++ provision/variables.tf
@@ -0,0 +1,11 @@
+variable aws_profile {
+  description = "The AWS profile name set in the credentials file"
+}
+
+variable aws_region {
+  description = "The AWS region"
+}
+
+variable aws_role_arn {
+  description ="The ARN of the role to assume"
+}
